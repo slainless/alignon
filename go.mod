f10d07dbdcad6efe8d5ba5a728ecd33ea337842b
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-21 09:52:39 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/slainless/my-alignon
+
+go 1.21.0
---
+++ scripts/install_bin.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./scripts/install_bin_migrate.sh
+./scripts/install_bin_dotenv.sh
+./scripts/install_bin_jet.sh
+./scripts/install_bin_swag.sh
